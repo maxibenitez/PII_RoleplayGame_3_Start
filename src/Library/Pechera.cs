2b5144d355b4d916483f59c37fc4843740ebb4e5
Salonso1602 <santiago.ralonso@correo.ucu.edu.uy>
2021-09-22 20:27:06 -0300

    abreviado setter y getter

--- src/Library/Pechera.cs
+++ src/Library/Pechera.cs
@@ -2,22 +2,9 @@ namespace Program
 {
     public class Pechera : IItem: IDefense
     {
-        private int _def;
-
         public string Name {get; set;}
 
-        public int DEF 
-        {
-            get
-            {
-                return this._def;
-            }
-
-            private set
-            {
-                this._def = value;
-            } 
-        }
+        public int DEF {get; private set;}
         
         public Item (string nombre, int def)
         {
